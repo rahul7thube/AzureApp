010ddbaeabd88f3f11736cd5cf27fe4b22494262
rahul7thube <148743030+rahul7thube@users.noreply.github.com>
2024-01-05 17:57:24 +0530

    Update install-playbook.sh

--- ansible-playbooks/install-playbook.sh
+++ ansible-playbooks/install-playbook.sh
@@ -11,12 +11,16 @@ sudo add-apt-repository --yes --update ppa:ansible/ansible
 
 # Install Ansible
 sudo apt-get install ansible -y
+# Configure inventory file
+echo "[webservers]" > inventory
+IP=$(curl -s ifconfig.me)
+echo $IP >> inventory
 
 # Download the Ansible playbook
 # Replace the URL with the location of your Ansible playbook
 wget https://github.com/rahul7thube/AzureApp/raw/main/ansible-playbooks/configure-webserver.yml
 
 # Run the Ansible playbook
-ansible-playbook configure-webserver.yml
+ansible-playbook -i inventory configure-webserver.yml
 
 # Optional: Add any additional commands or scripts here
